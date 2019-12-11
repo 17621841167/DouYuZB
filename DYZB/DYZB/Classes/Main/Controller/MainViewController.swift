414d8138a2e16d922dd686728cbf707a2577ba1b
huitailang947 <541051323@qq.com>
2019-12-11 13:17:20 +0800

    add icon

---
+++ DYZB/DYZB/Classes/Main/Controller/MainViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MainViewController.swift
+//  DYZB
+//
+//  Created by wenyimeng on 2019/12/11.
+//  Copyright © 2019 liuhangjun. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            addChildVc(storyname: "Home")
+            addChildVc(storyname: "Live")
+         addChildVc(storyname: "Follow")
+         addChildVc(storyname: "Profile")
+
+    }
+    
+
+  
+    private func addChildVc(storyname : String)
+    {
+        //1.通过sb获取控制器
+        let childVc = UIStoryboard(name: storyname, bundle: nil).instantiateInitialViewController()!
+           
+          
+        //2.将childVc作为自控制器
+        
+        addChild(childVc)
+    }
+}
