a280b7943e6b77769ad812c501ea6b1ccd76f715
huitailang947 <541051323@qq.com>
2019-12-11 12:04:47 +0800

    tijao

--- DYZB/DYZB/AppDelegate.swift
+++ DYZB/DYZB/AppDelegate.swift
@@ -16,9 +16,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         window.frame = UIScreen.main.bounds
-        let vc = HomeViewController()
-        window.rootViewController = UINavigationController.init(rootViewController: vc)
+//        let vc = HomeViewController()
+//        window.rootViewController = UINavigationController.init(rootViewController: vc)
+//        window.makeKeyAndVisible()
+        
+        
+        let mainStoryBoard = UIStoryboard.init(name: "Main", bundle: nil)
+        
+        let vc = mainStoryBoard.instantiateViewController(withIdentifier: "Root")
+   
+
+        window.rootViewController = vc;
         window.makeKeyAndVisible()
+    
         return true
     }
 
